eec4a0eab5c8502a81835cd329c9e6c479163126
KsaweryKapela <ksawerykapela97@gmail.com>
2022-08-09 13:33:39 +0200

    main page front end done for now

---
+++ static/js/base.js
@@ -0,0 +1,27 @@
+const RecommendNavbar = document.getElementsByClassName('nav-item-1')[0];
+const MovieBaseNavbar = document.getElementsByClassName('nav-item-2')[0];
+const MyMoviesNavbar = document.getElementsByClassName('nav-item-3')[0];
+
+var urls = ['/recommend', '/moviebase', '/']
+var allNavbar = document.getElementsByClassName('nav-item');
+
+if (document.URL.split('/')[document.URL.split('/').length - 1] == 'moviebase'){
+MovieBaseNavbar.classList.add('item-disabled');}
+else if((document.URL.split('/')[document.URL.split('/').length - 1] == 'recommend')){
+RecommendNavbar.classList.add('item-disabled');
+}
+else{MyMoviesNavbar.classList.add('item-disabled');
+console.log(MyMoviesNavbar.classList)}
+
+
+for(var i = 0; i < allNavbar.length; i++)
+{
+  allNavbar[i].addEventListener('click', GoToUrL.bind(i));
+
+  function GoToUrL(){
+window.location.href = urls[this]}
+}
+
+
+
+
